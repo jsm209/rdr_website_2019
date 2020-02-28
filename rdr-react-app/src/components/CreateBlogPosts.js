bcaf3d7f975e3b34d213dfe554ac4680e27f9eab
Andrew <acheung8@uw.edu>
2020-02-28 12:34:15 -0800

    Linked backend to CreateBlogPosts

--- rdr-react-app/src/components/CreateBlogPosts.js
+++ rdr-react-app/src/components/CreateBlogPosts.js
@@ -5,53 +5,49 @@ import BlogPost from './BlogPost';
 // This component will get all the profile information for the current shows
 // and display them as ShowCards.
 class CreateBlogPosts extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      data: []
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			data: []
+		}
+	}
 
-  // Will call all the data for DJ profiles.
-  componentDidMount() {
-    "https://api.tumblr.com/v2/blog/rainydawgradioblog.tumblr.com/info?api_key=AQaA9wiPTVjkSM5dBfvRwsM0rsY8pkIfuKmOEnhrH6Ip3oyPNQ"
-    let baseURL = "https://api.tumblr.com/v2/blog/";
-    let identifier = "rainydawgradioblog.tumblr.com/";
-    let method = "posts";
-    let api_key = "AQaA9wiPTVjkSM5dBfvRwsM0rsY8pkIfuKmOEnhrH6Ip3oyPNQ"; // hella unsafe but idfc
-    this.getData(baseURL + identifier + method + "?api_key=" + api_key);
-  }
+	// Will call all the data for DJ profiles.
+	componentDidMount() {
+		let baseURL = "http://rdr-backend.herokuapp.com/getTumblrInfo";
+		this.getData(baseURL);
+	}
 
-  // Will make a call to the Spinitron v2 API for 1 page of JSON data about
-  // show profiles.
-  getData(url) {
-    console.log(url);
-    fetch(url)
-    .then(response => response.json())
-    .then(data => this.setState({ data: data.response.posts }, () => {
-      console.log(this.state)
-    }))
-    .catch(console.log)
-  }
+	// Will make a call to the Spinitron v2 API for 1 page of JSON data about
+	// show profiles.
+	getData(url) {
+		console.log(url); // we might not need this hehe console lowkey cluttered
+		fetch(url)
+			.then(response => response.json())
+			.then(data => this.setState({ data: data.response.posts }, () => {
+				console.log(this.state)
+			}))
+			.catch(console.log)
+	}
 
-  render() {
-    let posts = this.state.data.map((blog) => {
-      return <BlogPost post={blog}/>
-    });
+	render() {
+		let posts = this.state.data.map((blog) => {
+			return <BlogPost post={blog}/>
+		});
 
-    let styles = {
-      display: "flex",
-      flexWrap: "wrap",
-      width: "350px",
-      justifyContent: "space-evenly"
-    }
+		let styles = {
+			display: "flex",
+			flexWrap: "wrap",
+			width: "350px",
+			justifyContent: "space-evenly"
+		}
 
-    return(
-      <div style={styles}>
-        {posts.slice(0, 4)}
-      </div>
-    );
-  }
+		return(
+			<div style={styles}>
+			{posts.slice(0, 4)}
+			</div>
+		);
+	}
 }
 
 export default CreateBlogPosts
