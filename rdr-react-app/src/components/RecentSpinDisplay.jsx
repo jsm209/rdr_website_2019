5aacc851f0eb13615de77ef876a3aeadb32bfcc1
Joshua Maza <joshuasadirimaza@gmail.com>
2020-03-04 18:04:50 -0800

    small tweaks

---
+++ rdr-react-app/src/components/RecentSpinDisplay.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+// React Bootstrap Components
+import { Navbar } from 'react-bootstrap';
+
+class RecentSpinDisplay extends React.Component{
+
+    constructor() {
+        super()
+        this.state = {
+            data: {},
+            isLoading: false
+        }
+    }
+
+    // Will create a repeating call that refreshes the currently playing song every interval.
+    componentDidMount() {
+        let waitTime = 10000; // the time in ms to wait before getting the most recent song again.
+
+        // Calls it initially, then sets an interval for it.
+        this.getData("https://spinitron.com/api/spins?access-token=VIT_hdbWICcgF3nGwvcLJCf6");
+        this.interval = setInterval(() => this.getData("https://spinitron.com/api/spins?access-token=VIT_hdbWICcgF3nGwvcLJCf6"), waitTime);
+        
+    }
+
+    // Removes repeating call after component is unmounted.
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    // Will make a call to the Spinitron v2 API
+    getData = (url) => {
+        let proxyurl = "https://cors-anywhere.herokuapp.com/";
+        return new Promise((resolve, reject) => fetch(proxyurl + url)      
+          .then(response => {
+            response.json().then(data => {
+              console.log(data);
+              this.setState((prevState) => {
+                return {
+                  data: data.items[0], //Only going to store the most recent song
+                  isLoading: true
+                }
+              })
+            }).catch(reject)
+          }).catch(reject)
+        )
+    }
+
+    render() {
+
+        let pStyle = {
+            paddingLeft: '20px', 
+            paddingTop: '10px', 
+            fontSize: '15px'
+        }
+
+        return(
+          <div>
+            <p>Recent Spin:</p>
+            <p style={pStyle}>San Holo - Honest</p>
+            <p style={pStyle}>time date here</p>
+          </div>
+        )
+    }
+
+    tConvert(time) {
+        // Check correct time format and split into components
+        time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+      
+        if (time.length > 1) { // If time format correct
+          time = time.slice (1);  // Remove full string match value
+          time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+          time[0] = +time[0] % 12 || 12; // Adjust hours
+        }
+        return time.join (''); // return adjusted time or original string
+      }
+}
+export default RecentSpinDisplay;
--- rdr-react-app/src/screens/HomeScreen.jsx
+++ rdr-react-app/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import CreateBlogPosts from '../components/CreateBlogPosts';
+import RecentSpinDisplay from '../components/RecentSpinDisplay';
 import { Timeline } from 'react-twitter-widgets'
 
 class HomeScreen extends React.Component {
@@ -31,37 +32,65 @@ class HomeScreen extends React.Component {
 		}
 
 		let screenFlexboxStyle = {
-			display: "flex"
+			display: "flex",
+			paddingLeft: "5%"
+		}
+
+		let newsContainer = {
+			paddingLeft: "20px"
+		}
+
+		let imageStyle = {
+			width: "150px",
+			height: "auto"
 		}
 	
 		return(
 			<div>
 				<h1 style={title}></h1>
+				<div style={screenFlexboxStyle}>
+					<div>
+						<img style={imageStyle} src='./images/rdr-logo.png'/>
+					</div>
+					<div style={newsContainer}>
+						<h2>News and Upcoming Events</h2>
+						<ListGroup flush>
+							<ListGroupItem>(3/6/2020) New website up!</ListGroupItem>
+							<ListGroupItem>(3/5/2020) Rock The Vote Show</ListGroupItem>
+						</ListGroup>
+					</div>
+					<div>
+						<RecentSpinDisplay />
+					</div>
+				</div>
 				<div style={blogPostContainerStyle}>
-					<div style={screenFlexboxStyle}>
+					<div>
 						<div>
 							<p style={blogPostContainerLable}>Most Recent Blog Posts</p>
 							<CreateBlogPosts />
 						</div>
-						<div>
-							<Timeline
-								dataSource={{
-								sourceType: 'profile',
-								screenName: 'RainyDawgRadio'
-								}}
-								options={{
-								username: 'RainyDawgRadio',
-								height: '400',
-								width: '200px'
-								}}
-								onLoad={() => console.log('Timeline is loaded!')}
-							/>
-						</div>
+
 					</div>
-
-
 				</div>
-				
+				<div style={screenFlexboxStyle}>
+					<div>
+						<Timeline
+							dataSource={{
+							sourceType: 'profile',
+							screenName: 'RainyDawgRadio'
+							}}
+							options={{
+							username: 'RainyDawgRadio',
+							height: '400',
+							width: '200px'
+							}}
+							onLoad={() => console.log('Timeline is loaded!')}
+						/>
+					</div>
+					<div>
+						<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FRainyDawgRadio%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+					</div>
+				</div>
 			</div>
 		)
 	}
